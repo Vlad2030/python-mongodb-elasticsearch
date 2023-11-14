beb926bf23a1c4844a3aa3b6c485d77521f5fccc
vladdd0 <vlad123123v@gmail.com>
2023-11-14 19:00:22 +0300

    Create migrate.py

---
+++ app/migrate.py
@@ -0,0 +1,52 @@
+# Migrate from MongoDB to ElasticSearch
+
+import datetime
+import json
+
+from elasticsearch import Elasticsearch, helpers
+from pymongo import MongoClient
+
+
+MONGO_DB_HOST = "172.21.0.2"
+MONGO_DB_PORT = 27017
+MONGO_DB_DATABASE = "pets"
+MONGO_DB_USERNAME = "root"
+MONGO_DB_PASSWORD = "toor"
+
+ELASTICSEARCH_DB_HOST = "192.168.1.65"
+ELASTICSEARCH_DB_PORT = 9200
+MELASTICSEARCH_DB_DATABASE = "pets"
+ELASTICSEARCH_DB_USERNAME = "root"
+ELASTICSEARCH_DB_PASSWORD = "toor"
+
+# Mongo Config
+client = MongoClient(f"mongodb://{MONGO_DB_HOST}:{MONGO_DB_PORT}")
+db = client[MONGO_DB_DATABASE]
+collection = db[MONGO_DB_DATABASE]
+
+# Elasticsearch Config
+es = Elasticsearch([f"http://{ELASTICSEARCH_DB_HOST}:{ELASTICSEARCH_DB_PORT}/"])
+es_index = MELASTICSEARCH_DB_DATABASE
+
+
+def defaultconverter(o):
+    if isinstance(o, datetime):
+        return o.__str__()
+
+
+def migrate():
+    res = collection.find()
+    # number of docs to migrate
+    num_docs = 2000
+    actions = []
+    for i in range(num_docs):
+        doc = res[i]
+        mongo_id = doc['_id']
+        doc = json.dumps(doc, default = defaultconverter)
+        doc.pop('_id', None)
+        actions.append({
+            "_index": es_index,
+            "_id": mongo_id,
+            "_source": json.dumps(doc)
+        })
+    helpers.bulk(es, actions)
